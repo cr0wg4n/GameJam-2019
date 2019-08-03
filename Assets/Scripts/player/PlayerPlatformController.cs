15245586fbe7f12d36f884c3ec33896eddc20ff7
cr0wg4n <mcm.crw@gmail.com>
2019-08-03 16:47:12 -0400

    escenario basico

--- Assets/Scripts/player/PlayerPlatformController.cs
+++ Assets/Scripts/player/PlayerPlatformController.cs
@@ -7,6 +7,10 @@ public class PlayerPlatformController : MonoBehaviour
     public float speed = 10f;
     float jump = 6.5f;
     private Rigidbody2D rgb2D;
+    private Vector3 backTime;
+    float clock = 0.0f;
+    public int goToBackTime = 1;
+    private int lastTime = 0;
     void Start()
     {
         rgb2D = GetComponent<Rigidbody2D>();
@@ -14,6 +18,7 @@ public class PlayerPlatformController : MonoBehaviour
 
     void Update()
     {
+        timeControl();
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             rgb2D.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
         }
@@ -22,6 +27,19 @@ public class PlayerPlatformController : MonoBehaviour
     {
         Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
         transform.position = Vector3.MoveTowards(transform.position, transform.position + mov, speed * Time.deltaTime);
-
+        if (Input.GetKeyDown(KeyCode.T)) {
+            transform.position = backTime;
+        }
+    }
+    public void timeControl() {
+        clock += Time.deltaTime;
+        if (lastTime != (int)clock % 60)
+        {
+            backTime = transform.position;
+        }
+        lastTime = (int)clock % 60;
+    }
+    public int getTime() {
+        return lastTime;
     }
 }
