e96aaae165d2dab8b364dc1e2cd6425ecc879dc1
cr0wg4n <mcm.crw@gmail.com>
2019-08-02 23:13:59 -0400

    mecanica de caminar listra

---
+++ Assets/Scripts/player/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position = Vector3.MoveTowards(transform.position,transform.position+mov,speed*Time.deltaTime);
+        
+    }
+}
